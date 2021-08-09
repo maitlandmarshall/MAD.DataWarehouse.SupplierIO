9f095a6320a7a2a8a68e2b8292790ad4b7b0563d
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-08-09 14:47:45 +1000

    Add project files.

---
+++ MAD.DataWarehouse.SupplierIO/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace MAD.DataWarehouse.SupplierIO
+{
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection serviceDescriptors)
+        {
+
+        }
+
+        public async Task Configure(IGlobalConfiguration hangfireConfig)
+        {
+
+        }
+    }
+}
